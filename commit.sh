b676a6fffd3bd7a813bfe85dbe06a87c539f299e
Paul Rafferty <raff@calypsoai.com>
2021-05-26 09:34:40 +0100

    Updating commit.sh with some echos - the docker commit results in a pretty huge image (11gb+) because of the extra nvidia packages it installs

--- commit.sh
+++ commit.sh
@@ -7,5 +7,13 @@ ID=$(docker container ls -a -q --filter name=sdkmanager)
 if [ -z "${ID}" ]; then
   echo "Could not find container 'sdkmanager' running. Did you exit?"
 else
+  echo "Saving the container 'sdkmanager' to the docker image 'sdkmanager:${USER}'..."
   docker commit sdkmanager sdkmanager:${USER}
+
+  echo
+
+  docker images | grep sdkmanager
+
+  echo
+  echo "You should not need to keep the sdkmanager:latest image"
 fi
